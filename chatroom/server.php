f1ddf0a9d054e3b4d4aacb8104eb10ee82b79bbe
lisiqiong <peterlee20100427@yeah.net>
2017-11-14 14:06:33 +0800

    test not use key

--- chatroom/server.php
+++ chatroom/server.php
@@ -5,16 +5,18 @@ $server = new swoole_websocket_server("0.0.0.0",9502);
 $server->on('open', function (swoole_websocket_server $server, $request) {
 	echo "server: handshake success with fd{$request->fd}\n";//$request->fd 是客户端id
 });
+
 $server->on('message', function (swoole_websocket_server $server, $frame) {
-echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+	echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
 
 //$frame->fd 是客户端id，$frame->data是客户端发送的数据
 //服务端向客户端发送数据是用 $server->push( '客户端id' ,  '内容')
 
-$data = $frame->data;
-foreach($server->connections as $fd){
-	$server->push($fd , $data);//循环广播
-}
+	$data = $frame->data;
+	foreach($server->connections as $fd){
+		$server->push($fd , $data);//循环广播
+	}
+	
 });
 
 $server->on('close', function ($ser, $fd) {
